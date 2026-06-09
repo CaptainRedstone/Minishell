bf9c16762c531fd0fb13ee6e89655bce1eccb726
Adrien Forcada <aforcada@c1r13s6.42lausanne.ch>
2026-06-09 16:58:34 +0200

    subject and progress in parser structure (ast)

--- includes/parser.h
+++ includes/parser.h
@@ -6,20 +6,56 @@
 /*   By: aforcada <aforcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 15:18:09 by aforcada          #+#    #+#             */
-/*   Updated: 2026/06/09 15:43:38 by aforcada         ###   ########.fr       */
+/*   Updated: 2026/06/09 16:57:01 by aforcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 typedef struct s_token	t_token;
+typedef struct s_cmd	t_cmd;
+typedef struct s_pipe	t_pipe;
+typedef struct s_redir	t_redir;
+typedef struct s_exec	t_exec;
 
 struct s_token
 {
-	char	*name;
 	int		type;
+	char	*name;
+	int		quote;
 	t_token	*next;
+};
+
+struct s_cmd
+{
+	int		type;
+	char	**args;
 };
 
 struct s_pipe
 {
-	
+	int		type;
+	t_cmd	*left;
+	t_cmd	*right;
+};
+
+struct s_redir
+{
+	int		type;
+	char	*path;
+	int		fd;
+	int		mode;
+	t_cmd	*cmd;
+};
+
+struct s_exec
+{
+	int		type;
+	char	**args;
+};
+
+enum e_token_type
+{
+	e_word,
+	e_pipe,
+	e_quote,
+	e_
 };
