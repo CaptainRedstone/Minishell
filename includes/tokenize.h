f384239bead06487fb778592876e72168e03cfe6
Adrien Forcada <aforcada@c3r3s3.42lausanne.ch>
2026-08-28 18:30:06 +0200

    update tokenize.h

--- includes/tokenize.h
+++ includes/tokenize.h
@@ -6,105 +6,68 @@
 /*   By: aforcada <aforcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/21 20:28:47 by aforcada          #+#    #+#             */
-/*   Updated: 2026/08/28 17:52:15 by aforcada         ###   ########.fr       */
+/*   Updated: 2026/08/28 18:24:57 by aforcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#ifndef	TOKENIZE_H
 
-#define TK_WORD_NAME "TK_WORD"
-#define TK_PIPE_NAME "TK_PIPE"
-#define TK_SQUOTE_NAME "TK_SQUOTE"
-#define TK_DQUOTE_NAME "TK_DQUOTE"
-#define TK_REDIR_IN_NAME "TK_REDIR_IN"
-#define TK_REDIR_OUT_NAME "TK_REDIR_OUT"
-#define TK_REDIR_APPEND_NAME "TK_REDIR_APPEND"
-#define TK_REDIR_HEREDOC_NAME "TK_REDIR_HEREDOC"
+# include "../includes/minishell.h"
 
-#define TK_PIPE_VAL "|"
-#define TK_SQUOTE_VAL "\'"
-#define TK_DQUOTE_VAL "\""
-#define TK_REDIR_IN_VAL "<"
-#define TK_REDIR_OUT_VAL ">"
-#define TK_REDIR_APPEND_VAL ">>"
-#define TK_REDIR_HEREDOC_VAL "<<"
+# define TKZ_MAXITER 10000
+# define TKZ_METACHARS "\'\"|<>"
 
-#define TK_METACHARS "\'\"|<>"
-#define TK_QUOTE_CHARS "\'\""
-#define TK_REDIR_CHARS "<>"
-#define TK_BLANK_CHARS " \t"
+# define TK_NULL_NAME "TK_NULL"
+# define TK_BLANK_NAME "TK_BLANK"
+# define TK_SQUOTE_NAME "TK_SQUOTE"
+# define TK_DQUOTE_NAME "TK_DQUOTE"
+# define TK_PIPE_NAME "TK_PIPE"
+# define TK_REDIR_IN_NAME "TK_REDIR_IN"
+# define TK_REDIR_OUT_NAME "TK_REDIR_OUT"
+# define TK_WORD_NAME "TK_WORD"
+
+# define TK_NULL_VAL '\0'
+# define TK_TAB_VAL '\t'
+# define TK_SPACE_VAL ' '
+# define TK_PIPE_VAL '|'
+# define TK_SQUOTE_VAL '\''
+# define TK_DQUOTE_VAL '\"'
+# define TK_REDIR_IN_VAL '<'
+# define TK_REDIR_OUT_VAL '>'
 
 enum e_token_type
 {
 	TK_NULL,
-	TK_WORD,
-	TK_PIPE,
+	TK_BLANK,
 	TK_SQUOTE,
 	TK_DQUOTE,
+	TK_PIPE,
 	TK_REDIR_IN,
 	TK_REDIR_OUT,
-	TK_REDIR_APPEND,
-	TK_REDIR_HEREDOC,
+	TK_WORD,
 	TK_END,
 };
 
 enum e_tokenizer_state
 {
-	TKZ_ERROR,
-	TKZ_SKIP,
+	TKZ_START,
 	TKZ_BUILD,
-	TKZ_APPEND,
 	TKZ_END,
 };
 
 typedef struct s_token
 {
 	int		type;
-	int		pos;
-	char	*val;
+	size_t	pos;
 	size_t	len;
 }	t_token;
 
 typedef struct s_tokenizer
 {
 	int		state;
-	size_t	pos;
-	int		(*skip_charset)(t_context *ctx, size_t *pos);
-	int		(*append_token)(t_context *ctx, t_token *token);
+	size_t	read_pos;
 	void	(*handle_err)(t_context *ctx);
 }	t_tokenizer;
-
-/**
- * @brief	Extract token from input line (ctx->line) and copies its
- * content and metadata (type + length) at the end of  the token list
- * (ctx->token_lst)
- * 
- * @param	ctx		Context with various placeholders, notably line
- * @param	start	Start index of token in line. 
- * @param	len		End index of token in line
- * @param	type	Type of token
- * 
- * @return	1 is success, 0 is failure
- */
-int		tk_extract(t_context *ctx, int start, size_t len, int type);
-
-/**
- * @brief	Compute len of token in ctx->line from start to char in stop_set
- * 
- * @param	ctx			Context with placeholders, notably `line` and `line_len`
- * @param	start		Start index in `line`
- * @param	stop_set	Set of chars, each represents the end of a token
- * 
- * @return	Length of token
- */
-int		tk_len(t_context *ctx, int start, int type, const char *stop_set);
-
-/**
- * @brief Return the name of token type
- * 
- * @param type token type, see `e_tk_type`
- */
-char	*tk_typename(int type);
 
 /**
  * @brief Print token to stdout
@@ -116,3 +79,5 @@ void	tk_print(void *content);
  * tokens ctx->token_lst then return 1 on success and 0 on failure.
  */
 int		tokenize(t_context *ctx);
+
+#endif
