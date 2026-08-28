52a888e264a05a09afca83758aff63cc10623c7a
Adrien Forcada <aforcada@c3r3s3.42lausanne.ch>
2026-08-28 18:33:25 +0200

    gitignore

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,17 +6,31 @@
 /*   By: aforcada <aforcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 10:43:09 by aforcada          #+#    #+#             */
-/*   Updated: 2026/08/28 17:55:29 by aforcada         ###   ########.fr       */
+/*   Updated: 2026/08/28 18:29:18 by aforcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/tokenize.h"
 
+// generic functions
+size_t	strlen_if(const char *str, int (*cond)(char c))
+{
+	size_t	i;
+	
+	i = 0;
+	while (i < TKZ_MAXITER && cond(str[i]))
+	i++;
+	return (i);
+}
+
+// token related
 void	tk_print(void *content)
 {
 	(void)content;
 	printf("je suis un token ma gueule\n");
 }
+
+// tokenize related
 
 int	tokenize(t_context *ctx)
 {
